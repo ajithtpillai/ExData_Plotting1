b0e2f9f724d261e28d7d19777529e5c00d6889d0
ajithtpillai <ajithtpillai@gmail.com>
2015-06-08 14:12:56 +0530

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -19,5 +19,5 @@ consump1$Day=sub("Friday","Fri",consump1$Day)
 with(consump1,plot(Datetime,Global_active_power,type="l",ylab="Global Active Power (kilowatts)",xlab=""))
 
 
-dev.copy(png,file="plot2.png")
+dev.copy(png,file="plot2.png",width=480,height=480)
 dev.off()
